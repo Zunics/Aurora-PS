4c85bab65b50bf65ab122406a35a479977fb50df
Zunics <kar0804@hotmail.com>
2017-07-10 20:03:15 -0500

    Create regdate.js

---
+++ chat-plugins/regdate.js
@@ -0,0 +1,72 @@
+'use strict';
+
+let fs = require('fs');
+let http = require('http');
+const Autolinker = require('autolinker');
+
+let regdateCache = {};
+
+EM.regdate = function (target, callback) {
+	target = toId(target);
+	if (regdateCache[target]) return callback(regdateCache[target]);
+	let options = {
+		host: 'pokemonshowdown.com',
+		port: 80,
+		path: '/users/' + target + '.json',
+		method: 'GET',
+	};
+	http.get(options, function (res) {
+		let data = '';
+		res.on('data', function (chunk) {
+			data += chunk;
+		}).on('end', function () {
+			data = JSON.parse(data);
+			let date = data['registertime'];
+			if (date !== 0 && date.toString().length < 13) {
+				while (date.toString().length < 13) {
+					date = Number(date.toString() + '0');
+				}
+			}
+			if (date !== 0) {
+				regdateCache[target] = date;
+				saveRegdateCache();
+			}
+			callback((date === 0 ? false : date));
+		});
+	});
+};
+
+// last two functions needed to make sure EM.regdate() fully works
+function loadRegdateCache() {
+	try {
+		regdateCache = JSON.parse(fs.readFileSync('config/regdate.json', 'utf8'));
+	} catch (e) {}
+}
+loadRegdateCache();
+
+function saveRegdateCache() {
+	fs.writeFileSync('config/regdate.json', JSON.stringify(regdateCache));
+}
+
+EM.parseMessage = function (message) {
+	if (message.substr(0, 5) === "/html") {
+		message = message.substr(5);
+		message = message.replace(/\_\_([^< ](?:[^<]*?[^< ])?)\_\_(?![^<]*?<\/a)/g, '<i>$1</i>'); // italics
+		message = message.replace(/\*\*([^< ](?:[^<]*?[^< ])?)\*\*/g, '<b>$1</b>'); // bold
+		message = message.replace(/\~\~([^< ](?:[^<]*?[^< ])?)\~\~/g, '<strike>$1</strike>'); // strikethrough
+		message = message.replace(/&lt;&lt;([a-z0-9-]+)&gt;&gt;/g, '&laquo;<a href="/$1" target="_blank">$1</a>&raquo;'); // <<roomid>>
+		message = Autolinker.link(message.replace(/&#x2f;/g, '/'), {stripPrefix: false, phone: false, twitter: false});
+		return message;
+	}
+	message = Chat.escapeHTML(message).replace(/&#x2f;/g, '/');
+	message = message.replace(/\_\_([^< ](?:[^<]*?[^< ])?)\_\_(?![^<]*?<\/a)/g, '<i>$1</i>'); // italics
+	message = message.replace(/\*\*([^< ](?:[^<]*?[^< ])?)\*\*/g, '<b>$1</b>'); // bold
+	message = message.replace(/\~\~([^< ](?:[^<]*?[^< ])?)\~\~/g, '<strike>$1</strike>'); // strikethrough
+	message = message.replace(/&lt;&lt;([a-z0-9-]+)&gt;&gt;/g, '&laquo;<a href="/$1" target="_blank">$1</a>&raquo;'); // <<roomid>>
+	message = Autolinker.link(message, {stripPrefix: false, phone: false, twitter: false});
+	return message;
+};
+
+EM.randomString = function (length) {
+	return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+};
